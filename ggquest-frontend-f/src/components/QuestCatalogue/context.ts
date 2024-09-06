350cdd62f03cac414cb5a6b066008689d8bcce08
Buidlor <89695544+Buidlor@users.noreply.github.com>
2024-09-06 17:12:55 +0200

    Deploy website - based on 286208607ccedc1fb3d2ff11739d1e7db92adeae

--- ggquest-frontend-f/src/components/QuestCatalogue/context.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const initialState = {
-  active: "overview",
-  setActive: (active: string) => {},
-};
-
-export const CardContext = React.createContext(initialState);
--- ggquest-frontend-f/src/pages/StatsPage.tsx
+++
@@ -1,9 +0,0 @@
-import StatsBody from "../components/StatsBody";
-
-const StatsPage = ()=> {
-    return (
-        <StatsBody />
-    )
-}
-
-export default StatsPage;
